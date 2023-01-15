d775b95b4963f8362cdf07ba5013642ac66a3058
Steve Waldman <swaldman@mchange.com>
2023-01-15 01:22:27 -0500

    Update untemplate version to 0.0.2

--- untemplatedoc/src/untemplatedoc/common.scala
+++ untemplatedoc/src/untemplatedoc/common.scala
@@ -61,4 +61,4 @@ def box( emoji : Option[String], label : Option[String] )( block : String ) =
   val newLinePart = if (emoji orElse label).nonEmpty then " <br/>" + LineSep else ""
   prependEachLine("> ")( emojiPart + labelPart + newLinePart + block)
 
-val untemplateVersion = "0.0.1-SNAPSHOT"
+val untemplateVersion = "0.0.2"
